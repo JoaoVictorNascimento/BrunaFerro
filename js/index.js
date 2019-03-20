6f45ecf5179a8112412d14cd5c2c5e61a4483a76
AnaFrozza <annafrozza@gmail.com>
2019-03-19 22:26:19 -0300

    subindo as telas do projeto

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function mostraDescricao(menu){
+    document.getElementById('descricao'+menu).style.display = '';
+}
+
+function someDescricao(menu){
+    document.getElementById('descricao'+menu).style.display = 'none';
+}
